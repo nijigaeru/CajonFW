810ec00af393df75bdd9fac0a4bd96b1016c5c0a
chack_j <chack.j3772@gmail.com>
2025-02-13 21:54:42 +0900

    作業中

--- CajonFW/CajonFW.ino
+++ CajonFW/CajonFW.ino
@@ -6,18 +6,6 @@
 #include "SLD.h"
 #include "SW.h"
 
-const char* ssid = "M5StampAP"; // アクセスポイントのSSID
-const char* password = "your_PASSWORD"; // アクセスポイントのパスワード
-
-WebServer server(80);
-
-const int ledPin = 2; // LEDが接続されているピン番号
-uint8_t g_ucLedState = 0;
-uint8_t g_ucDeviceConnected = 0;
-int display_rotation = 1;                         // 画面向きの初期化
-// LED制御のためのキュー
-QueueHandle_t ledQueue;
-
 
 void ledControlTask(void *pvParameters) {
   int command;
@@ -46,7 +34,7 @@ void setup() {
   //ledQueue = xQueueCreate(10, sizeof(int));
 
   // HTTPサーバータスクをCore 1で実行
-  // xTaskCreatePinnedToCore(httpServerTask, "HTTP Server Task", 4096, NULL, 1, NULL, 1);
+  xTaskCreatePinnedToCore(HTTPTask, "HTTPTask", 4096, NULL, 1, NULL, 1);
   // ファイル管理タスクの作成
   xTaskCreatePinnedToCore(FMGTask, "FMGTask", 2048, NULL, 1, NULL, 0);
   for (uint8_t ucFetCh = 1; ucFetCh <= 8; ucFetCh++) {
---
+++ CajonFW/HTTP.cpp
@@ -0,0 +1,75 @@
+#include <WiFi.h>
+#include <WebServer.h>
+#include "HTTP.h"
+/******** macro *****/
+
+
+/******** global variable  ***** */
+const char* ssid = "tobukaeru_Cajon"; // アクセスポイントのSSID
+const char* password = "tobukaeru_Cajon"; // アクセスポイントのパスワード
+
+// キューの定義
+QueueHandle_t g_pstHTTPQueue;
+AsyncWebServer server(80);
+
+/******** function declaration ***** */
+void HTTPTask(void* pvParameters);   //HTTPタスク
+
+/************************** */
+// HTTPタスク
+
+void HTTTPTask(void* pvParameters){
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+
+  Serial.println("Connected to WiFi");
+
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/html", htmlPage);
+  });
+
+  server.on("/prevTrack", HTTP_GET, [](AsyncWebServerRequest *request){
+    Serial.println("Previous Track");
+    request->send(200, "text/plain", "Previous Track");
+  });
+
+  server.on("/startTrack", HTTP_GET, [](AsyncWebServerRequest *request){
+    Serial.println("Start Track");
+    request->send(200, "text/plain", "Start Track");
+  });
+
+  server.on("/nextTrack", HTTP_GET, [](AsyncWebServerRequest *request){
+    Serial.println("Next Track");
+    request->send(200, "text/plain", "Next Track");
+  });
+
+  server.on("/stopTrack", HTTP_GET, [](AsyncWebServerRequest *request){
+    Serial.println("Stop Track");
+    request->send(200, "text/plain", "Stop Track");
+  });
+
+  server.on("/playTrack", HTTP_GET, [](AsyncWebServerRequest *request){
+    String trackName;
+    if (request->hasParam("name")) {
+      trackName = request->getParam("name")->value();
+      Serial.println("Play " + trackName);
+    }
+    request->send(200, "text/plain", "Play " + trackName);
+  });
+
+  server.on("/seek", HTTP_GET, [](AsyncWebServerRequest *request){
+    String value;
+    if (request->hasParam("value")) {
+      value = request->getParam("value")->value();
+      Serial.println("Seek to: " + value);
+    }
+    request->send(200, "text/plain", "Seek to: " + value);
+  });
+
+  server.begin();
+}
